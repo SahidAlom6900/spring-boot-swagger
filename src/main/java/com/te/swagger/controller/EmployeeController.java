798bd8a7e9f0603c520cf9706f2757d5dcedd783
Sahidalom6900 <sahidalom6900@gmail.com>
2022-10-16 02:26:51 +0530

    Project added

---
+++ src/main/java/com/te/swagger/controller/EmployeeController.java
@@ -0,0 +1,83 @@
+package com.te.swagger.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.te.swagger.entity.Employee;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@RestController
+public class EmployeeController {
+
+	private List<Employee> employees = createList();
+
+	@Operation(summary = "This is to fetch All the Participantss stored in Db")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Details of All the Participants", content = {
+					@Content(mediaType = "application/json") }),
+			@ApiResponse(responseCode = "404", description = "Page not found", content = @Content) })
+	@GetMapping("/employees")
+	public List<Employee> firstPage() {
+		return employees;
+	}
+
+	@Operation(summary = "Get a book by its id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found the book", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Employee.class)) }),
+			@ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Book not found", content = @Content) })
+	@Parameter(description = "id of book to be searched")
+	@DeleteMapping(path = { "/{id}" })
+	public ResponseEntity<?> delete(@PathVariable("id") int id) {
+		Employee deletedEmp = null;
+		for (Employee emp : employees) {
+			if (emp.getEmpId().equals(id)) {
+				employees.remove(emp);
+				deletedEmp = emp;
+				break;
+			}
+		}
+
+		return  ResponseEntity.ok(1);
+	}
+
+	@PostMapping
+	public Employee create(@RequestBody Employee user) {
+		employees.add(user);
+		System.out.println(employees);
+		return user;
+	}
+
+	private static List<Employee> createList() {
+		List<Employee> tempEmployees = new ArrayList<>();
+		Employee emp1 = new Employee();
+		emp1.setName("emp1");
+		emp1.setDesignation("manager");
+		emp1.setEmpId("1");
+		emp1.setSalary(3000);
+
+		Employee emp2 = new Employee();
+		emp2.setName("emp2");
+		emp2.setDesignation("developer");
+		emp2.setEmpId("2");
+		emp2.setSalary(3000);
+		tempEmployees.add(emp1);
+		tempEmployees.add(emp2);
+		return tempEmployees;
+	}
+}
